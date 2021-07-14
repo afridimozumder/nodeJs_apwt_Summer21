e668340229f4989632c616be28f170215e1e6e49
Afridi Mozumder <49424469+afridimozumder@users.noreply.github.com>
2021-07-15 01:27:13 +0600

    ES6 basics

---
+++ basic/ES6/index.js
@@ -0,0 +1,94 @@
+"use strict"
+
+//const student = ['afridi', 'anonna', 'dhaka', 'delhi'];
+const num = ['1', '2', '3', '4'];
+
+/* const newarray = student.filter(function(std){
+    return std[0] == 'a';
+});
+//alternative
+const newarray = student.filter(std=>std[0]=='d'); */
+
+/* const newarray = student.join('--');
+
+console.log(newarray); */
+
+/* const newarray = [...student, '#', ...num];
+console.log(newarray); */
+
+//object destructuring -->
+/* const student = {id: 1, name: 'afridi', email: 'afridi@gmail.com'}
+const {id, name:myname, email:myemail} = student;
+console.log(myemail); */
+
+/* console.log('run now..');
+setTimeout(function(){
+    console.log('run after 3 sec...');
+}, 3000)
+setTimeout(function(){
+    console.log('run after 3 sec...');
+}, 3000)
+setTimeout(function(){
+    console.log('run after 5 sec...');
+}, 5000) */
+
+//promise object
+/* const p = new Promise((resolve, reject)=>{
+	let sum = 10;
+	if(sum == 10){
+		setTimeout(()=>{
+			resolve('true');			
+		}, 2000);
+	}else{
+		reject('false');
+	}
+});
+
+p.then(msg=>{
+    console.log(msg);
+}).catch(error=>{
+    console.log(error);
+}); */
+
+//function returns promise
+/* function sum(a, b){
+    return new Promise((resolve, reject)=>{
+        if(a!==0 && b!==0){
+
+            setTimeout(()=>{
+                let sum = a+b;
+                resolve(sum);
+            }, 3000);
+
+        }else{
+            reject(0);
+        }
+    })
+} */
+
+/* sum(1,9).then(msg=>{
+    console.log(msg);
+}).catch(error=>{
+    console.log(error);
+}); */
+
+//asynchronous function
+/* async function mytask (x, y){
+    let total = await sum(x, y);
+    return total;
+}
+
+mytask(1,9).then(msg=>{
+    console.log(msg);
+}).catch(error=>{
+    console.log(error);
+}); */
+
+
+import {Student} from './Student';
+
+
+const s1 = new student('afridi', 'abc@gmail.com', 'cse');
+console.log(s1.getName());
+
+
---
+++ basic/ES6/student.js
@@ -0,0 +1,11 @@
+export default class student{
+    constructor(name, email, dept){
+        this.name = name;
+        this.email = email;
+        this.dept = dept;
+    }
+
+    getName(){
+        return this.name;
+    }
+}
